78171532f1c8a98b5bc8ca7161daed5c45dc5a41
Mary Alice Orr <m.a.orr17@gmail.com>
2018-05-05 11:04:22 -0500

    got dom elements working and added buttons and search bar and styling

--- javascripts/data.js
+++ javascripts/data.js
@@ -1,9 +1,17 @@
-// const dom = require('./dom');
+const dom = require('./dom');
 const loadEx = require ('./ex');
-// const loadFish = require('./locations');
+const loadLocations = require('./locations');
 
 const whenExLoads = (data) => {
+  const domEx = dom.printEx(data.ex);
   console.log('data', data);
+  console.log('domEx', domEx);
+  $('#ex').html(domEx);
+};
+
+const whenLocationLoads = (data) => {
+  console.log('data', data);
+  $('#locations').html(dom.printLocations(data.locations));
 };
 
 const nope = (error) => {
@@ -12,6 +20,7 @@ const nope = (error) => {
 
 const goTime = () => {
   loadEx(whenExLoads, nope);
+  loadLocations(whenLocationLoads, nope);
 };
 
 module.exports = goTime;
