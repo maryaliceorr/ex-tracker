530795aacecaf2fd0fdbe4f7a80a2b131fa39c13
Mary Alice Orr <m.a.orr17@gmail.com>
2018-05-05 14:58:34 -0500

    wip commit

--- javascripts/events.js
+++ javascripts/events.js
@@ -1,40 +1,56 @@
+// to uppercase
+
+jQuery.expr[':'].icontains = function (a, i, m) {
+  return jQuery(a).text().toUpperCase()
+    .indexOf(m[3].toUpperCase()) >= 0;
+};
+
 // Button Events
 
 const filterMorning = () => {
-  $('.time').not('.morning').closest('.location').addClass('hide');
-  $('.time').filter('.morning').closest('.location').removeClass('hide');
+  $('.time').not('.morning').closest('.location').hide();
+  $('.time').filter('.morning').closest('.location').show();
 };
 
 const filterAfternoon = () => {
-  $('.time').not('.afternoon').closest('.location').addClass('hide');
-  $('.time').filter('.afternoon').closest('.location').removeClass('hide');
+  $('.time').not('.afternoon').closest('.location').hide();
+  $('.time').filter('.afternoon').closest('.location').show();
 };
 
 const filterEvening = () => {
-  $('.time').not('.evening').closest('.location').addClass('hide');
-  $('.time').filter('.evening').closest('.location').removeClass('hide');
+  $('.time').not('.evening').closest('.location').hide();
+  $('.time').filter('.evening').closest('.location').show();
 };
 
 const filterAfterDark = () => {
-  $('.time').not('.after-dark').closest('.location').addClass('hide');
-  $('.time').filter('.after-dark').closest('.location').removeClass('hide');
+  $('.time').not('.after-dark').closest('.location').hide();
+  $('.time').filter('.after-dark').closest('.location').show();
 };
 
-// const reset = () => {
-//   $('.location').removeClass('hide');
-// };
+const filterReset = () => {
+  $('.location').show();
+};
 
 const buttonClicks = () => {
   $('#morning-button').click(filterMorning);
   $('#afternoon-button').click(filterAfternoon);
   $('#evening-button').click(filterEvening);
   $('#after-dark-button').click(filterAfterDark);
+  $('#reset-button').click(filterReset);
 };
-
-module.exports =  buttonClicks;
 
 // Search bar Events
 
-// const searchInput = () => {
-//   $('#search')
-// }
+const searchInput = () => {
+  $('#search-input').val();
+};
+
+const matchCards = (locations) => {
+  $('#searchbutton').click();
+  $(`'.location':not(:icontains(searchInput))`).hide();
+};
+
+module.exports =  {
+  buttonClicks,
+  matchCards,
+};
